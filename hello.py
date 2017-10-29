1d92199b57c4acebe71bd0d0056d42820d9179be
Namir Hassan <namirhassan@live.com>
2017-10-29 02:53:05 -0600

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+def main():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    sumnum = num1 + num2
+    print("Sum = {}".format(sumnum))
+    return 0
+
+
+
+if __name__ == "__main__":
+    main()
